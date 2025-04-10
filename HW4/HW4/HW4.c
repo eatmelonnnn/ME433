96982935d5fa4c8359429c17fd7cea81c0d563b4
eatmelonnnn <yiranhuang2026@u.northwestern.edu>
2025-04-10 14:54:10 -0500

    Update HW4.c

--- HW4/HW4/HW4.c
+++ HW4/HW4/HW4.c
@@ -24,9 +24,13 @@ static inline void cs_deselect(uint cs_pin) {
     asm volatile("nop \n nop \n nop"); // FIXME
 }
 
-void writeDac(){
+void writeDac(int channel, float voltage){
     uint8_t data[2];
     int len = 2;
+    uint16_t d = 0;
+    d = d | (channel<<15);
+    d = d |0b111 << 12;
+    uint16_t v = voltage * 
     data [0] = 0b01010101;
     data [1] = 255;
     cs_select(PIN_CS);
@@ -38,7 +42,7 @@ int main()
     stdio_init_all();
 
     // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000);
+    spi_init(SPI_PORT, 20000);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
@@ -52,6 +56,14 @@ int main()
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
+        int i = 0;
+        float t = 0;
+        for (i=0; i < 100; i++){
+            t += 0.1;
+            float v = sin(t)
+            writeDac(0,v);
+            sleep_ms(10);
+        }
         writeDac();
     }
 }
