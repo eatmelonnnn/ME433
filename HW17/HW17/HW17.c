f4410885b7d8f562d5d862578c93040ab1e4de86
eatmelonnnn <yiranhuang2026@u.northwestern.edu>
2025-06-05 13:09:51 -0500

    HW17 Updates

--- HW17/HW17/HW17.c
+++ HW17/HW17/HW17.c
@@ -3,21 +3,18 @@
 #include "hardware/pwm.h"
 #include "cam.h"
 
-// === Motor H-Bridge PWM Pins ===
 #define AIN1 18
 #define AIN2 19
 #define BIN1 20
 #define BIN2 21
 
 #define PWM_WRAP 6250  //  20kHz PWM
-
-// === Line Sensor Constants ===
 #define CAMERA_CENTER 40
 
 // === Control Parameters ===
 #define BASE_DUTY_PCT 75.0f  // % duty when going straight
 #define MAX_DUTY_PCT 100.0f
-#define GAIN 2.0f            // increase this for sharper turns
+#define GAIN 1.0f           
 #define DEAD_BAND 4
 
 void setup_pwm(uint gpio) {
@@ -28,11 +25,10 @@ void setup_pwm(uint gpio) {
     pwm_set_enabled(slice, true);
 }
 
-// Drive motor in forward/reverse using two pins
-// REVERSED: PWM forward is actually on the *reverse* pin
+
 void set_motor_pwm_percent(float duty_pct, uint pin_fwd, uint pin_rev) {
     if (duty_pct > 0.0f) {
-        pwm_set_gpio_level(pin_fwd, 0);  // previously forward
+        pwm_set_gpio_level(pin_fwd, 0); 
         pwm_set_gpio_level(pin_rev, (uint16_t)(PWM_WRAP * duty_pct / 100.0f));
     } else if (duty_pct < 0.0f) {
         pwm_set_gpio_level(pin_fwd, (uint16_t)(PWM_WRAP * -duty_pct / 100.0f));
@@ -44,7 +40,7 @@ void set_motor_pwm_percent(float duty_pct, uint pin_fwd, uint pin_rev) {
 }
 
 int main() {
-    stdio_init_all();  // optional for debugging
+    stdio_init_all();  
     init_camera_pins();
 
     setup_pwm(AIN1);
